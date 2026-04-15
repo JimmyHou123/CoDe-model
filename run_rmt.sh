c3009bb320ee91a616da78b7219b2805bf7a5d55
Jimmy Hou <122401153+JimmyHou123@users.noreply.github.com>
2026-04-14 17:04:57 -0700

    Update run_rmt.sh

--- run_rmt.sh
+++ run_rmt.sh
@@ -1,10 +1,11 @@
+# UPDATE THIS TO YOUR DATA PATH e.g. ".../data/HARDI150.nii.gz"
+# UPDATE THIS TO YOUR SAVING PATH e.g. ".../save/RMT_denoised"
+# UPDATE THIS TO YOUR VALID MASK START, default is 10
+# UPDATE THIS TO YOUR VALID MASK END, default is 160
+
 python dipy_mppca.py \
-  # UPDATE THIS TO YOUR DATA PATH e.g. ".../data/HARDI150.nii.gz"
   --data_root null \
-  # UPDATE THIS TO YOUR SAVING PATH e.g. ".../save/RMT_denoised"
   --save_dir null \
-  # UPDATE THIS TO YOUR VALID MASK START, default is 10
   --valid_mask_start 10 \
-  # UPDATE THIS TO YOUR VALID MASK END, default is 160
   --valid_mask_end 160 \
   --patch_radius 3
