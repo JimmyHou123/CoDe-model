3016954cc014723a425a108b5549823d7b7cdb5e
Jimmy Hou <122401153+JimmyHou123@users.noreply.github.com>
2026-04-17 14:05:12 -0700

    Update run_stage2.sh

--- run_stage2.sh
+++ run_stage2.sh
@@ -1,14 +1,23 @@
-# Cell 2: Stage 2 DWI consistency‐model training
+#!/bin/bash
+# Cell 2: Stage 2 DWI consistency-model training
+# UPDATE data_root TO YOUR DATA PATH e.g. ".../data/HARDI150.nii.gz"
+# If you do not intend to use the RMT regularizer, set the loss_norm option to l1, l2, or lpips
+# UPDATE save_dir TO YOUR SAVING PATH e.g. ".../save/stageII"
+# UPDATE noisemodel_checkpoint TO THE STAGE I CHECKPOINT PATH e.g. ".../save/stageI/model150000.pt"
+# UPDATE valid_mask_start TO YOUR VALID MASK START, default is 10
+# UPDATE valid_mask_end TO YOUR VALID MASK END, default is 160
+# UPDATE rmt_npy_path TO THE RMT DENOISED ARRAY e.g. ".../rmt_denoised.npy"
+
 mpiexec -n 1 --allow-run-as-root python -m scripts.cm_train_stage2_dwi \
-  --data_root null \ // UPDATE THIS TO YOUR DATA PATH e.g. ".../data/HARDI150.nii.gz" 
+  --data_root null \
   --training_mode consistency_training \
   --target_ema_mode adaptive \
   --start_ema 0.95 \
   --scale_mode progressive \
   --start_scales 2 \
   --end_scales 150 \
-  --total_training_steps 800000 \
-  --loss_norm lpips_rmt \ // #If you do not intend to use the RMT regularizer, set the option to l1, l2, or lpips
+  --total_training_steps 300000 \
+  --loss_norm lpips_rmt \
   --lr_anneal_steps 0 \
   --attention_resolutions 16 \
   --use_scale_shift_norm False \
@@ -29,12 +38,14 @@ mpiexec -n 1 --allow-run-as-root python -m scripts.cm_train_stage2_dwi \
   --use_fp16 True \
   --weight_decay 0.0 \
   --weight_schedule uniform \
-  --save_dir null \ // UPDATE THIS TO YOUR SAVING PATH e.g. ".../save/stageII"
+  --save_dir null \
   --save_interval 10000 \
-  --epoch 427 \
+  --epoch 53 \
   --channel_mult 1,2,4,8,8 \
   --sigma_min 0.0002 \
   --sigma_max 2 \
-  --noisemodel_checkpoint null \ // UPDATE THIS TO THE STAGE I CHECKPOINT PATH e.g. ".../save/stageI/model150000.pt" 
+  --noisemodel_checkpoint null \
   --log_interval 500 \
-  --rmt_npy_path null \ // UPDATE THIS TO THE RMT DENOISED ARRAY e.g. ".../rmt_denoised.npy"
+  --valid_mask_start 10 \
+  --valid_mask_end 160 \
+  --rmt_npy_path null
