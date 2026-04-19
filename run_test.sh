032723cd67479c6caee5a9664eac51d9da765400
Jimmy Hou <122401153+JimmyHou123@users.noreply.github.com>
2026-04-19 12:43:46 -0700

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,16 @@
+# Cell 3: Stage 2 DWI consistency-model testing
+# UPDATE data_root TO YOUR DATA PATH e.g. ".../data/HARDI150.nii.gz"
+# UPDATE valid_mask TO YOUR VALID MASK START AND END, default is 10 160
+# UPDATE resume_checkpoint TO THE STAGE II CHECKPOINT PATH e.g. ".../save/stageII/model300000.pt"
+# UPDATE noisemodel_checkpoint TO THE STAGE I CHECKPOINT PATH e.g. ".../save/stageI/model150000.pt"
+# UPDATE save_dir TO YOUR SAVING PATH FOR INFERENCE e.g. ".../save/inference"
+
 python -m scripts.cm_test_stage2_dwi \
-  --data_root null \ // UPDATE THIS TO YOUR DATA PATH e.g. ".../data/HARDI150.nii.gz"
+  --data_root null \
   --valid_mask 10 160 \
-  --resume_checkpoint null \ // UPDATE THIS TO THE STAGE II CHECKPOINT PATH e.g. ".../save/stageII/model300000.pt" 
-  --noisemodel_checkpoint null \ // UPDATE THIS TO THE STAGE I CHECKPOINT PATH e.g. ".../save/stageI/model150000.pt" 
-  --save_dir null \ // UPDATE THIS TO YOUR SAVING PATH FOR INFERENCE e.g. ".../save/inference"
+  --resume_checkpoint null \
+  --noisemodel_checkpoint null \
+  --save_dir null \
   --image_size 128 \
   --in_channels 1 \
   --out_channels 1 \
